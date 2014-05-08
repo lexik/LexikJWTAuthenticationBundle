f88f3d1322dc38b78ead753725a8971fae2181ba
jaugustin <jeremie.augustin@pixel-cookers.com>
2014-05-08 17:57:10 +0200

    Add JWTEntryPoint to throw 401 when no token are sent

---
+++ Security/Http/EntryPoint/JWTEntryPoint.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Lexik\Bundle\JWTAuthenticationBundle\Security\Http\EntryPoint;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+/**
+ * JWTEntryPoint starts throw a 401 when not authenticated.
+ *
+ * @author Jérémie Augustin <jeremie.augustin@pixel-cookers.com>
+ */
+class JWTEntryPoint implements AuthenticationEntryPointInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new Response('', 401);
+    }
+}
