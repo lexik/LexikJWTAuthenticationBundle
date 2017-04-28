ce719385410b0db74e6bbdf00cc0d0efb46c7411
Robin Chalas <robin.chalas@gmail.com>
2017-04-28 10:36:45 +0200

    Fix encoder autowiring for symfony <3.3

--- Tests/Functional/DependencyInjection/AutowiringTest.php
+++ Tests/Functional/DependencyInjection/AutowiringTest.php
@@ -5,8 +5,10 @@ namespace Lexik\Bundle\JWTAuthenticationBundle\Tests\Functional\DependencyInject
 use Lexik\Bundle\JWTAuthenticationBundle\DependencyInjection\LexikJWTAuthenticationExtension;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\DefaultEncoder;
 use Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWSProvider\DefaultJWSProvider;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTManager;
 use Lexik\Bundle\JWTAuthenticationBundle\Tests\Stubs\Autowired;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\ChainTokenExtractor;
 use Symfony\Bundle\FrameworkBundle\DependencyInjection\FrameworkExtension;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
@@ -19,18 +21,7 @@ class AutowiringTest extends \PHPUnit_Framework_TestCase
 {
     public function testAutowiring()
     {
-        $container = new ContainerBuilder(new ParameterBag([
-            'kernel.bundles'          => ['FrameworkBundle' => FrameworkBundle::class, 'LexikJWTAuthenticationBundle' => LexikJWTAuthenticationBundle::class],
-            'kernel.bundles_metadata' => [],
-            'kernel.cache_dir'        => __DIR__,
-            'kernel.debug'            => false,
-            'kernel.environment'      => 'test',
-            'kernel.name'             => 'kernel',
-            'kernel.root_dir'         => __DIR__,
-            'kernel.container_class'  => 'AutowiringTestContainer',
-            'kernel.charset'          => 'utf8',
-        ]));
-
+        $container = $this->createContainerBuilder();
         $container->registerExtension(new SecurityExtension());
         $container->registerExtension(new FrameworkExtension());
         $container->registerExtension(new LexikJWTAuthenticationExtension());
@@ -47,5 +38,53 @@ class AutowiringTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInstanceOf(JWTManager::class, $autowired->getJWTManager());
         $this->assertInstanceOf(DefaultEncoder::class, $autowired->getJWTEncoder());
+        $this->assertInstanceOf(ChainTokenExtractor::class, $autowired->getTokenExtractor());
+        $this->assertInstanceOf(DefaultJWSProvider::class, $autowired->getJWSProvider());
+    }
+
+    public function testAutowireConfiguredEncoderServiceForInterfaceTypeHint()
+    {
+        if (!method_exists(ContainerBuilder::class, 'fileExists')) {
+            $this->markTestSkipped('Using the configured encoder for autowiring is supported using symfony 3.3+ only.');
+        }
+
+        $container = $this->createContainerBuilder();
+        $container->registerExtension(new SecurityExtension());
+        $container->registerExtension(new FrameworkExtension());
+        $container->registerExtension(new LexikJWTAuthenticationExtension());
+
+        (new YamlFileLoader($container, new FileLocator([__DIR__.'/../app/config'])))->load('config_custom_encoder.yml');
+
+        $container
+            ->register('autowired', Autowired::class)
+            ->setAutowired(true);
+
+        $container->compile();
+
+        $autowired = $container->get('autowired');
+
+        $this->assertInstanceOf(DummyEncoder::class, $autowired->getJWTEncoder());
+    }
+
+    private static function createContainerBuilder()
+    {
+        return new ContainerBuilder(new ParameterBag([
+            'kernel.bundles'          => ['FrameworkBundle' => FrameworkBundle::class, 'LexikJWTAuthenticationBundle' => LexikJWTAuthenticationBundle::class],
+            'kernel.bundles_metadata' => [],
+            'kernel.cache_dir'        => __DIR__,
+            'kernel.debug'            => false,
+            'kernel.environment'      => 'test',
+            'kernel.name'             => 'kernel',
+            'kernel.root_dir'         => __DIR__,
+            'kernel.container_class'  => 'AutowiringTestContainer',
+            'kernel.charset'          => 'utf8',
+        ]));
+    }
+}
+
+final class DummyEncoder extends DefaultEncoder
+{
+    public function __construct()
+    {
     }
 }
--- Tests/Stubs/Autowired.php
+++ Tests/Stubs/Autowired.php
@@ -3,18 +3,24 @@
 namespace Lexik\Bundle\JWTAuthenticationBundle\Tests\Stubs;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWSProvider\JWSProviderInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTManager;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\TokenExtractorInterface;
 
 class Autowired
 {
     private $jwtManager;
     private $jwtEncoder;
+    private $tokenExtractor;
+    private $jwsProvider;
 
-    public function __construct(JWTTokenManagerInterface $jwtManager, JWTEncoderInterface $jwtEncoder)
+    public function __construct(JWTTokenManagerInterface $jwtManager, JWTEncoderInterface $jwtEncoder, TokenExtractorInterface $tokenExtractor, JWSProviderInterface $jwsProvider)
     {
         $this->jwtManager = $jwtManager;
         $this->jwtEncoder = $jwtEncoder;
+        $this->tokenExtractor = $tokenExtractor;
+        $this->jwsProvider = $jwsProvider;
     }
 
     public function getJWTManager()
@@ -25,5 +31,15 @@ class Autowired
     public function getJWTEncoder()
     {
         return $this->jwtEncoder;
+    }
+
+    public function getTokenExtractor()
+    {
+        return $this->tokenExtractor;
+    }
+
+    public function getJWSProvider()
+    {
+        return $this->jwsProvider;
     }
 }
