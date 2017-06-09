e30bde8a52c60e692b1d70dd0b4748d247a3ce7b
Samuel ROZE <samuel.roze@gmail.com>
2017-06-09 16:27:51 +0100

    #344 Add an extension point on the UserProviderWithPayloadSupportInterface (sroze)

---
+++ Security/User/UserProviderWithPayloadSupportsInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Lexik\Bundle\JWTAuthenticationBundle\Security\User;
+
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+interface UserProviderWithPayloadSupportsInterface extends UserProviderInterface
+{
+    /**
+     * Load a user by its username, including the JWT token payload.
+     *
+     * @param string $username
+     * @param array  $payload
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     *
+     * @return UserInterface
+     */
+    public function loadUserByUsernameAndPayload($username, array $payload);
+}
