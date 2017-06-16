60f0dd7c218c0fc5bfaee05360027f3e674a4a3b
Spomky <Spomky@users.noreply.github.com>
2017-06-16 10:07:35 +0200

    The event now receives the JWT Header

--- Event/JWTCreatedEvent.php
+++ Event/JWTCreatedEvent.php
@@ -13,21 +13,44 @@ class JWTCreatedEvent extends Event
     /**
      * @var array
      */
-    protected $data;
+    protected $header;
 
+    /**
+     * @var array
+     */
+    protected $data;
+    
     /**
      * @var UserInterface
      */
     protected $user;
 
     /**
+     * @param array         $header
      * @param array         $data
      * @param UserInterface $user
      */
-    public function __construct(array $data, UserInterface $user)
+    public function __construct(array $header, array $data, UserInterface $user)
     {
+        $this->header = $header;
         $this->data = $data;
         $this->user = $user;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeader()
+    {
+        return $this->header;
+    }
+
+    /**
+     * @param array $header
+     */
+    public function setHeader(array $header)
+    {
+        $this->header = $header;
     }
 
     /**
