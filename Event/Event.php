0d564b3f7416650f038f03e0abd3249ecde5397d
Robin Chalas <robin.chalas@gmail.com>
2019-04-17 14:46:08 +0200

    Re-add needed use statements

--- Event/Event.php
+++ Event/Event.php
@@ -2,7 +2,9 @@
 
 namespace Lexik\Bundle\JWTAuthenticationBundle\Event;
 
+use Symfony\Component\EventDispatcher\Event as BaseEvent;
 use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Contracts\EventDispatcher\Event as ContractsBaseEvent;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 if (is_subclass_of(EventDispatcher::class, EventDispatcherInterface::class)) {
