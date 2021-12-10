c3dfa3afdc10722ba9083865a7f1b91789af4dfb
Marcel Berteler <pluk77@gmail.com>
2021-12-10 09:26:17 +0200

    Update LexikJWTAuthenticationExtension.php
    Added validate_tokens_without_ttl

--- DependencyInjection/LexikJWTAuthenticationExtension.php
+++ DependencyInjection/LexikJWTAuthenticationExtension.php
@@ -66,6 +66,7 @@ class LexikJWTAuthenticationExtension extends Extension
         $container->setParameter('lexik_jwt_authentication.token_ttl', $config['token_ttl']);
         $container->setParameter('lexik_jwt_authentication.clock_skew', $config['clock_skew']);
         $container->setParameter('lexik_jwt_authentication.user_identity_field', $config['user_identity_field']);
+        $container->setParameter('lexik_jwt_authentication.validate_tokens_without_ttl', $config['validate_tokens_without_ttl']);
 
         $user_id_claim = $config['user_id_claim'] ?: $config['user_identity_field'];
         $container->setParameter('lexik_jwt_authentication.user_id_claim', $user_id_claim);
