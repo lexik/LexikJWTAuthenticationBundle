lexik/LexikJWTAuthenticationBundle

commit 02a7bc03cf545695c086f300d58e5610d5888871

Allow lcobucci/jwt v5

Services/JWSProvider/LcobucciJWSProvider.php
@@ -69,11 +69,6 @@ class LcobucciJWSProvider implements JWSProviderInterface
     private $allowNoExpiration;
 
     /**
-     * @var bool
-     */
-    private $useDateObjects;
-
-    /**
      * @throws \InvalidArgumentException If the given crypto engine is not supported
      */
     public function __construct(KeyLoaderInterface $keyLoader, string $cryptoEngine, string $signatureAlgorithm, ?int $ttl, ?int $clockSkew, bool $allowNoExpiration = false, Clock $clock = null)
@@ -82,7 +77,7 @@ class LcobucciJWSProvider implements JWSProviderInterface
             throw new \InvalidArgumentException(sprintf('The %s provider supports only "openssl" as crypto engine.', self::class));
         }
         if (null === $clock) {
-            $clock = SystemClock::fromUTC();
+            $clock = new SystemClock(new \DateTimeZone('UTC'));
         }
 
         $this->keyLoader = $keyLoader;
@@ -91,7 +86,6 @@ class LcobucciJWSProvider implements JWSProviderInterface
         $this->ttl = $ttl;
         $this->clockSkew = $clockSkew;
         $this->allowNoExpiration = $allowNoExpiration;
-        $this->useDateObjects = method_exists(Token::class, 'payload') || class_exists(Plain::class);  // exists only on lcobucci/jwt 3.4+
     }
 
     /**
@@ -106,7 +100,7 @@ class LcobucciJWSProvider implements JWSProviderInterface
         }
 
         foreach ($header as $k => $v) {
-            $jws->withHeader($k, $v);
+            $jws = $jws->withHeader($k, $v);
         }
 
         $now = time();
@@ -114,28 +108,28 @@ class LcobucciJWSProvider implements JWSProviderInterface
         $issuedAt = $payload['iat'] ?? $now;
         unset($payload['iat']);
 
-        $jws->issuedAt($this->useDateObjects && !$issuedAt instanceof \DateTimeImmutable ? new \DateTimeImmutable("@{$issuedAt}") : $issuedAt);
+        $jws = $jws->issuedAt(!$issuedAt instanceof \DateTimeImmutable ? new \DateTimeImmutable("@{$issuedAt}") : $issuedAt);
 
         if (null !== $this->ttl || isset($payload['exp'])) {
             $exp = $payload['exp'] ?? $now + $this->ttl;
             unset($payload['exp']);
 
             if ($exp) {
-                $jws->expiresAt($exp instanceof \DateTimeImmutable ? $exp : ($this->useDateObjects ? new \DateTimeImmutable("@$exp") : $exp));
+                $jws = $jws->expiresAt(!$exp instanceof \DateTimeImmutable ? new \DateTimeImmutable("@{$exp}") : $exp);
             }
         }
 
         if (isset($payload['sub'])) {
-            $jws->relatedTo($payload['sub']);
+            $jws = $jws->relatedTo($payload['sub']);
             unset($payload['sub']);
         }
 
         if (interface_exists(RegisteredClaims::class)) {
-            $this->addStandardClaims($jws, $payload);
+            $jws = $this->addStandardClaims($jws, $payload);
         }
 
         foreach ($payload as $name => $value) {
-            $jws->withClaim($name, $value);
+            $jws = $jws->withClaim($name, $value);
         }
 
         $e = $token = null;
@@ -159,25 +153,18 @@ class LcobucciJWSProvider implements JWSProviderInterface
         }
 
         $payload = [];
-
-        if (!$this->useDateObjects) {
-            foreach ($jws->getClaims() as $claim) {
-                $payload[$claim->getName()] = $claim->getValue();
+        foreach ($jws->claims()->all() as $name => $value) {
+            if ($value instanceof \DateTimeInterface) {
+                $value = $value->getTimestamp();
             }
-        } else {
-            foreach ($jws->claims()->all() as $name => $value) {
-                if ($value instanceof \DateTimeInterface) {
-                    $value = $value->getTimestamp();
-                }
-                $payload[$name] = $value;
-            }
+            $payload[$name] = $value;
         }
 
         $jws = new LoadedJWS(
             $payload,
             $this->verify($jws),
             false == $this->allowNoExpiration,
-            $this->useDateObjects ? $jws->headers()->all() : $jws->getHeaders(),
+            $jws->headers()->all(),
             $this->clockSkew
         );
 
@@ -230,29 +217,6 @@ class LcobucciJWSProvider implements JWSProviderInterface
 
     private function verify(Token $jwt)
     {
-        if (!$this->useDateObjects) {
-            if (!$jwt->validate(new ValidationData(time() + $this->clockSkew))) {
-                return false;
-            }
-
-            if ($this->signer instanceof Hmac) {
-                return $jwt->verify(
-                    $this->signer,
-                    $this->keyLoader->loadKey(RawKeyLoader::TYPE_PRIVATE)
-                );
-            }
-
-            if (!empty($keys = $this->keyLoader->getAdditionalPublicKeys())) {
-                foreach ($keys as $key) {
-                    if ($jwt->verify($this->signer, $key)) {
-                        return true;
-                    }
-                }
-
-                return false;
-            }
-        }
-
         if (class_exists(InMemory::class)) {
             $key = InMemory::plainText($this->signer instanceof Hmac ? $this->keyLoader->loadKey(RawKeyLoader::TYPE_PRIVATE) : $this->keyLoader->loadKey(RawKeyLoader::TYPE_PUBLIC));
         } else {
@@ -289,7 +253,7 @@ class LcobucciJWSProvider implements JWSProviderInterface
         return false;
     }
 
-    private function addStandardClaims(Builder $builder, array &$payload)
+    private function addStandardClaims(Builder $builder, array &$payload): Builder
     {
         $mutatorMap = [
             RegisteredClaims::AUDIENCE => 'permittedFor',
@@ -311,7 +275,9 @@ class LcobucciJWSProvider implements JWSProviderInterface
                 continue;
             }
 
-            $builder->{$mutator}($value);
+            $builder = $builder->{$mutator}($value);
         }
+
+        return $builder;
     }
 }

commit 5d238ebc22eee36d40aab8690716766f70fc8d08

Fix missing array claims BC break in 2.9.0
Regression from 02a7bc03cf545695c086f300d58e5610d5888871
Fixes #1139

Services/JWSProvider/LcobucciJWSProvider.php
@@ -271,7 +271,7 @@ class LcobucciJWSProvider implements JWSProviderInterface
             unset($payload[$claim]);
 
             if (\is_array($value)) {
-                \call_user_func_array([$builder, $mutator], $value);
+                $builder = \call_user_func_array([$builder, $mutator], $value);
                 continue;
             }
 

Tests/Functional/GetTokenTest.php
@@ -58,7 +58,7 @@ class GetTokenTest extends TestCase
 
         $subscriber = static::$kernel->getContainer()->get('lexik_jwt_authentication.test.jwt_event_subscriber');
         $subscriber->setListener(Events::JWT_CREATED, function (JWTCreatedEvent $e) {
-            $e->setData($e->getData() + ['custom' => 'dummy']);
+            $e->setData($e->getData() + ['custom' => 'dummy', 'aud' => ['foo', 'bar']]);
             $e->setHeader($e->getHeader() + ['foo' => 'bar']);
         });
 
@@ -78,6 +78,7 @@ class GetTokenTest extends TestCase
         $jws = $parser->parse($token);
 
         $this->assertArrayHasKey('foo', method_exists($jws, 'headers') ? $jws->headers()->all() : $jws->getHeaders(), 'The payload should contains a custom "foo" header.');
+        $this->assertSame(['foo', 'bar'], $jws->claims()->get('aud'));
     }
 
     public function testGetTokenFromInvalidCredentials()